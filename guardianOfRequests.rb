e0841079a284fa78a5af51e63be02f77743dd025
andrei-garcia <andrei_garcia2@hotmail.com>
2016-02-29 20:53:59 -0300

    adiconados novos metodos e refatoração

--- guardianOfRequests.rb
+++ guardianOfRequests.rb
@@ -3,37 +3,72 @@ require 'sinatra'
 require 'httpclient'
 require 'nokogiri'
 
-real = 'localhost'
+def consultaUrl(uri,query,headers)
+	cliente = HTTPClient.new
+	cliente.get(uri,query,headers)
+end
 
-def getUrl(uri,query,headers)
-	clnt = HTTPClient.new
-	clnt.get(uri,query,headers)
+def ehHTML(contentType)
+	contentType == 'text/html'
+end
+
+def temElemento(html,elemento)
+	if(Nokogiri::HTML(html).at(elemento).nil?)
+		return false
+	end
+	true	
+end
+
+def criaInputHidden()
+	Nokogiri::HTML::Document.new.create_element "input",:type => "hidden",:name => "token"
+end
+
+def insereElemento(html,elemento)
+	documentoHTML = Nokogiri::HTML(html)
+	documentoHTML.root.at("//form").add_next_sibling elemento
+	return documentoHTML
+end
+
+def geraToken()
+	"falta gerar o token"
+end
+
+def atualizaHtmlComToken(document,option = {})
+	#ver como passar os options para definir encoding e outras coisas
+	document.to_html(:indent => 2,:encoding => 'UTF-8')
 end
 
 
 get '/*' do
 
-	#problemas de encodin, tem que passar o encodin correto no metodo to_html
+	#problemas de encoding, tem que passar o encodin correto no metodo to_html
 	#lembrar de testar de nao devolveu nulo quando procurar por formulário no metodo at
-	q = request.query_string
+
+	query = request.query_string
 	path = request.path
-	con = getUrl("http://#{real}/#{path}",q,headers)
-	
-	conteudo = con.body
+	host = request.host
 
-	if(con.headers['Content-Type']=='text/html')
-		 documentoHTML = Nokogiri::HTML(conteudo)
-		 inputHidden = documentoHTML.create_element "input"
-		 inputHidden.set_attribute 'type','hidden'
-		 inputHidden.set_attribute 'name','token'
-		 inputHidden.set_attribute 'value','aqui é o valor do token,falta gerar um token'
+	consulta = consultaUrl("http://#{host}/#{path}",query,headers)	
+	conteudo = consulta.body
 
-		 documentoHTML.root.at("//form").add_next_sibling inputHidden
+	if(ehHTML "#{consulta.headers['Content-Type']}")
 
-		 conteudo = documentoHTML.to_html(:indent => 5,:encoding => 'UTF-8')
+		if(temElemento(conteudo,"form"))
+			input = criaInputHidden
+			token = geraToken
+			input.set_attribute 'value', token
+			documentoHTML = insereElemento(conteudo,input)
+		 	conteudo = atualizaHtmlComToken(documentoHTML)
+		end	
+		 #documentoHTML = Nokogiri::HTML(conteudo)
+		 #inputHidden = documentoHTML.create_element "input"
+		 #inputHidden.set_attribute 'type','hidden'
+		 #inputHidden.set_attribute 'name','token'
+		 #inputHidden.set_attribute 'value','aqui é o valor do token,falta gerar um token'
+		 #documentoHTML.root.at("//form").add_next_sibling "<input value='teste'>"
 	end	
 
-	headers con.headers
+	headers consulta.headers
    	body << conteudo
 end
 
