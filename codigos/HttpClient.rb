9e3b35a4709bc5df3f5468897fcb0b97e43358d0
Andrei <andrei_garcia2@hotmail.com>
2016-06-24 19:53:07 -0300

    excluindo arquivos

--- codigos/HttpClient.rb
+++
@@ -1,9 +0,0 @@
-require 'httpclient'
-
-def getUrl(uri)
-	clnt = HTTPClient.new
-	return clnt.get(uri).body
-end
-#con = getUrl("http://#{real}/#{path}",q,headers)
-#puts getUrl("http://google.com.br")
-puts getUrl("http:/localhost/teste.html")
--- codigos/helo.rb
+++
@@ -1,5 +0,0 @@
-require 'rack'
-require 'httpclient'
-
-app = ->(env) { [200, {'Content-Type' => 'text/html'}, [ env.to_s ]] }
-Rack::Server.start app: app, server: 'webrick'
